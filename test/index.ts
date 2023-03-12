94e3460f5158c061afd7b796f694a6613ca722c4
Attila Max Ruf <therufa@gmail.com>
2023-03-12 18:27:48 +0100

    install standardjs linter

--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,18 @@
-import test from "ava";
-import fs from "fs";
-import path from "path";
-import convert from "../src/index";
+import fs from 'node:fs'
+import path from 'node:path'
+import test from 'ava'
+// eslint-disable-next-line import/extensions
+import convert from '../src/index'
 
 const style = fs.readFileSync(
-  path.resolve(__dirname, "./mockStyle.css"),
-  "utf8"
-);
+  // eslint-disable-next-line unicorn/prefer-module
+  path.resolve(__dirname, './mockStyle.css'),
+  'utf8'
+)
 
-test("convert() does convert", (t) => {
-  const result = convert(style);
-  console.log(result);
+test('convert() does convert', (t) => {
+  const result = convert(style)
+  console.log(result)
 
-  t.truthy(result);
-});
+  t.truthy(result)
+})
