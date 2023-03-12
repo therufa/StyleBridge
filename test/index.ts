3fc9520b4d1d8cb24dc31159035bf2649303dade
Attila Max Ruf <therufa@gmail.com>
2023-03-12 18:39:39 +0100

    install standardjs

--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,14 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import test from 'ava'
-// eslint-disable-next-line import/extensions
 import convert from '../src/index'
 
 const style = fs.readFileSync(
-  // eslint-disable-next-line unicorn/prefer-module
   path.resolve(__dirname, './mockStyle.css'),
   'utf8'
 )
 
-test('convert() does convert', (t) => {
+test('convert() does convert', t => {
   const result = convert(style)
   console.log(result)
 
