a570adc4b7127a92bfd3499accb9817c8d602dca
Attila Max Ruf <therufa@gmail.com>
2023-03-12 18:15:59 +0100

    make tests

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,16 @@
-import fs from "fs";
 import test from "ava";
+import fs from "fs";
+import path from "path";
 import convert from "../src/index";
 
-const style = fs.readFileSync("./mockStyle.css", "utf8");
+const style = fs.readFileSync(
+  path.resolve(__dirname, "./mockStyle.css"),
+  "utf8"
+);
 
-test("convert() does convert", () => {
+test("convert() does convert", (t) => {
   const result = convert(style);
   console.log(result);
+
+  t.truthy(result);
 });
